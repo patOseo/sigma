6a879455ca7913685a102cc85528c03a4c6c6b26
Pat Monette <patrick@ontarioseo.ca>
2023-10-06 15:18:06 -0400

    team block updates

--- blocks/team-members/block-team-members.php
+++ blocks/team-members/block-team-members.php
@@ -26,34 +26,46 @@ if($show_all) {
 }
 
 $members = new WP_Query($args);
+$num = $members->post_count;
 
+if($num >= 3) {
+    $col = '4';
+} elseif($num == 2) {
+    $col = '6';
+} else {
+    $col = '12';
+}
 ?>
 
 <div class="<?php echo esc_attr($class_name); ?>">
 <?php if($members->have_posts()): ?>
     <div class="row">
         <?php while($members->have_posts()): $members->the_post(); ?>
-            <div class="col-<?php echo $show_bio ? '12' : '6'; ?> col-md-4">
-                <div class="team-member position-relative mb-4 mb-lg-6">
-                    <div class="team-member-image text-center">
-                        <?php
-                        if(get_field('image', get_the_ID())) {
-                            $img_id = get_field('image', get_the_ID()); 
-                        } else {
-                            $img_id = 10611;
-                        }
-                        echo wp_get_attachment_image($img_id, array(300, 300)); 
-                        ?>
-                    </div>
-                    <a href="<?php echo get_permalink(); ?>" class="stretched-link">
-                        <div class="position-relative py-3 text-center">
-                            <div class="d-inline-block p-3 bg-white">
-                                <h2 class="h5 mb-0 text-uppercase"><?php echo get_the_title(); ?></h2>
-                                <h3 class="h6 mb-0 text-uppercase text-grey"><?php echo get_field('position', get_the_ID()); ?></h3>
-                            </div>
+            <div class="col-<?php echo $show_bio ? '12' : '6'; ?> col-md-<?php echo $col; ?>">
+                <div class="team-member position-relative mb-4 mb-lg-6 row">
+                    <div class="col-12 <?php if($num == 1) { echo 'col-lg-4'; } ?>">
+                        <div class="team-member-image text-center">
+                            <?php
+                            if(get_field('image', get_the_ID())) {
+                                $img_id = get_field('image', get_the_ID()); 
+                            } else {
+                                $img_id = 10611;
+                            }
+                            echo wp_get_attachment_image($img_id, array(300, 300)); 
+                            ?>
                         </div>
-                    </a>
-                    <?php if($show_bio) { echo '<div class="px-4 fs-sm text-start">' . get_field('biography', get_the_ID()) . '</div>'; } ?>
+                    </div>
+                    <div class="col-12 <?php if($num == 1) { echo 'col-lg-8'; } ?>">
+                        <a href="<?php echo get_permalink(); ?>" class="stretched-link">
+                            <div class="position-relative py-3 <?php if($num == 1) { echo 'text-start'; } else { echo 'text-center'; } ?>">
+                                <div class="d-inline-block p-3 bg-white">
+                                    <h2 class="h5 mb-0 text-uppercase"><?php echo get_the_title(); ?></h2>
+                                    <h3 class="h6 mb-0 text-uppercase text-grey"><?php echo get_field('position', get_the_ID()); ?></h3>
+                                </div>
+                            </div>
+                        </a>
+                        <?php if($show_bio) { echo '<div class="px-4 fs-sm text-start">' . get_field('biography', get_the_ID()) . '</div>'; } ?>
+                    </div>
                 </div>
             </div>
         <?php endwhile; ?>
