cd7dd900534a52043786457b71f1a72744a2c884
Pat Monette <patrick@ontarioseo.ca>
2025-11-19 14:44:06 -0500

    shadowbox carousel update for larger images

--- blocks/shadowbox-carousel/block-shadowbox-carousel.php
+++ blocks/shadowbox-carousel/block-shadowbox-carousel.php
@@ -20,8 +20,25 @@ $prevclass = 'swiper-button-prev-' . $uid;
                 <div class="swiper-slide d-flex align-items-center text-center h-100">
                     <div class="shadowbox-block w-100 h-100">
                         <div class="slider-image d-flex flex-column align-self-center text-center w-100">
-                            <?php $image = get_sub_field('icon'); ?>
-                            <?php echo wp_get_attachment_image($image, array(60, 60), '', array('class' => 'mx-auto mb-4')); ?>
+                            <?php 
+                            $image = get_sub_field('icon');
+                            $size = get_sub_field('image_size');
+                            $image_size = 'full';
+                            if($size == 'original') {
+                                $image_size = 'large';
+                            }elseif($size == 'icon') {
+                                $image_size = array(60, 60);
+                            }
+                            if(get_sub_field('aspect_ratio') != 'original') {
+                                $ratio = 'mb-4 rounded-4 overflow-hidden ratio ratio-' . get_sub_field('aspect_ratio');
+                            } else {
+                                $ratio = 'rounded-4 original';
+                            }
+
+                            ?>
+                            <div class="<?php echo $ratio; ?>">
+                                <?php if($image): echo wp_get_attachment_image($image, $image_size, '', array('class' => 'mx-auto mb-4 object-fit-cover')); endif; ?>
+                            </div>
                             <div class="h4"><?php echo get_sub_field('title'); ?></div>
                             <div><?php echo get_sub_field('description'); ?></div>
                         </div>
